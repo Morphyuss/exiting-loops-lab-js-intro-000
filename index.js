1f14e41b690fcba369282c779fddc36dbacc38b3
James Stamps Jr <jstampshere@gmail.com>
2020-03-06 23:13:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function breakOut(array, changeValue, stopValue) {
 return array;
 } 
   
-
+/*
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; 1 < array.length; i++) {
     if (array[i] === skipValue);
@@ -18,7 +18,7 @@ function keepGoing(array, changeValue, skipValue) {
   }
 return array;
   
-}
+} */
 
 
 function findBy(array, findFn) {

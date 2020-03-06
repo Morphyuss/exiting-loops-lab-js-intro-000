a2c14b2d2b66db8fb8ff20d2769915385bb2b1ca
James Stamps Jr <jstampshere@gmail.com>
2020-03-06 23:16:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,12 +13,15 @@ return array;
 
 function keepGoing(array, changeValue, skipValue) {
   for (let i = 0; i < array.length; i++) {
-    if (array[i] === skipValue);
+    if (array[i] === skipValue) {
     continue;
+  } else{
+    array[i] = changeValue;
   }
 return array;
   
 } 
+}
 
 
 function findBy(array, findFn) {

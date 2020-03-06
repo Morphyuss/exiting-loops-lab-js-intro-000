06697e795c013241ce16967f14ee202f5aeacaae
James Stamps Jr <jstampshere@gmail.com>
2020-03-06 23:06:56 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function breakOut(array, changeValue, stopValue) {
+ for (let i = 0; i < array.length; i++) {
+  if (array[i] === stopValue) {
+     break;
+  }
+    
+    array[i] = changeValue;
+}  
+return array;
+} 
+  
+
+function keepGoing(array, changeValue, skipValue) {
+  for (let i = 0; 1 < array.length; i++) {
+    if (array[i] === skipValue);
+    continue;
+  }
+return array;
+  
+}
+
+
+function findBy(array, findFn) {
+  find([array], (s) => s.length);}
+
+
+
+
+
+
+
+
